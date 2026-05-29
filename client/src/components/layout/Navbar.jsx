64bf1601aca8d8579ac2e7b44d507a411d639481
DavidCevallos15 <jimdav1506ceva@gmail.com>
2026-05-29 10:46:48 -0500

    feat: actualizar boton de POA a POA 2026 con enlace directo

--- client/src/components/layout/Navbar.jsx
+++ client/src/components/layout/Navbar.jsx
@@ -25,7 +25,7 @@ const navLinks = [
       { label: 'Noticias y Actualidad', to: '/noticias' },
       { label: 'Documentos y Transparencia', to: '/documentos' },
       { label: 'GeoSalud MSP', to: '/subcentros' },
-      { label: 'POA', to: `${API_BASE}/public/poa/download` },
+      { label: 'POA 2026', to: 'http://drive.hpvc.gob.ec/owncloud/index.php/s/RzphFTzamzyzLxN' },
     ]
   },
   { 
